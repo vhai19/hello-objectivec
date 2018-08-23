29182a7fe2921c9458991167bc0539e4380daf6b
Nguyen Van Hai <n.vanhai19@gmail.com>
2018-08-24 00:03:40 +0900

    [HelloObjectiveC]feat HelloViewController

---
+++ hello-objective/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  hello-objective
+//
+//  Created by Nguyen Van Hai on 2018/08/24.
+//  Copyright © 2018 Carmate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
---
+++ hello-objective/HelloWorldViewController.m
@@ -0,0 +1,37 @@
+//
+//  HelloWorldViewController.m
+//  hello-objective
+//
+//  Created by Nguyen Van Hai on 2018/08/24.
+//  Copyright © 2018 Carmate. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
